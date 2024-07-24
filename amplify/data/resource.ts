08955a62575ca591f2dac2a05571981fa7138689
gou_tanaka <gou_tanaka@nissay-it.co.jp>
2024-07-24 14:20:33 +0900

    スキーマ追加

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -12,6 +12,12 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
+  Test: a
+    .model({
+      booleanContent: a.boolean(),
+      stringContent: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
